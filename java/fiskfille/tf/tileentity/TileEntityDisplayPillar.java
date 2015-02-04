8426fd0711249c3b30df928095dc2eeabd0ec57d
gegy1000 <gegy1000@gmail.com>
2015-02-04 15:59:21 +0200

    Woops!

--- java/fiskfille/tf/tileentity/TileEntityDisplayPillar.java
+++ java/fiskfille/tf/tileentity/TileEntityDisplayPillar.java
@@ -6,8 +6,6 @@ import net.minecraft.network.NetworkManager;
 import net.minecraft.network.Packet;
 import net.minecraft.network.play.server.S35PacketUpdateTileEntity;
 import net.minecraft.tileentity.TileEntity;
-import fiskfille.tf.TransformersMod;
-import fiskfille.tf.packet.PacketSyncDisplayPillar;
 
 public class TileEntityDisplayPillar extends TileEntity
 {
