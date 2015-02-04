785cb923fb23afd479a10b486dc8d86c5047e71f
gegy1000 <gegy1000@gmail.com>
2015-02-04 18:19:57 +0200

    Fix arm showing when you are a transformer!

--- java/fiskfille/tf/render/entity/RenderCustomPlayer.java
+++ java/fiskfille/tf/render/entity/RenderCustomPlayer.java
@@ -2,8 +2,10 @@ package fiskfille.tf.render.entity;
 
 import net.minecraft.client.renderer.entity.RenderPlayer;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
+import fiskfille.tf.TFHelper;
 import fiskfille.tf.data.TFDataManager;
 
 @SideOnly(Side.CLIENT)
@@ -17,7 +19,16 @@ public class RenderCustomPlayer extends RenderPlayer
 	@Override
 	public void renderFirstPersonArm(EntityPlayer player)
 	{
-		if (!(TFDataManager.isInVehicleMode(player) && TFDataManager.getTransformationTimer(player) == 0))
+		ItemStack currentArmour = player.getCurrentArmor(2);
+		
+		if(currentArmour != null)
+		{
+			if (!TFHelper.isTransformerArmor(player, currentArmour.getItem()))
+			{
+				super.renderFirstPersonArm(player);
+			}
+		}
+		else
 		{
 			super.renderFirstPersonArm(player);
 		}
