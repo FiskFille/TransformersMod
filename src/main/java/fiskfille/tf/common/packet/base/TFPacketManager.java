0cf7c51922b2b9308e4a1e7ce308c960df03bb22
gegy1000 <gegy1000@gmail.com>
2015-02-20 13:25:59 +0200

    No Fisk, you dont need a packet there

--- src/main/java/fiskfille/tf/common/packet/base/TFPacketManager.java
+++ src/main/java/fiskfille/tf/common/packet/base/TFPacketManager.java
@@ -9,7 +9,6 @@ import fiskfille.tf.common.packet.PacketBroadcastState;
 import fiskfille.tf.common.packet.PacketBroadcastStealthState;
 import fiskfille.tf.common.packet.PacketBroadcastTransformationState;
 import fiskfille.tf.common.packet.PacketCloudtrapJetpack;
-import fiskfille.tf.common.packet.PacketFlamethrowerFire;
 import fiskfille.tf.common.packet.PacketHandleStealthTransformation;
 import fiskfille.tf.common.packet.PacketHandleTransformation;
 import fiskfille.tf.common.packet.PacketSyncTransformationStates;
@@ -36,7 +35,6 @@ public class TFPacketManager
 		registerPacket(PacketBroadcastState.Handler.class, PacketBroadcastState.class);
 		registerPacket(PacketVehicleNitro.Handler.class, PacketVehicleNitro.class);
 		registerPacket(PacketVurpSniperShoot.Handler.class, PacketVurpSniperShoot.class);
-		registerPacket(PacketFlamethrowerFire.Handler.class, PacketFlamethrowerFire.class);
 	}
 
     private static <REQ extends IMessage, REPLY extends IMessage> void registerPacket(Class<? extends IMessageHandler<REQ, REPLY>> messageHandler, Class<REQ> requestMessageType)
