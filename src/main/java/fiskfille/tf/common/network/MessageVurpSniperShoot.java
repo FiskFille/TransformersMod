3f1f1273d51ceafc3d16b49a40cc08fb3a5f093d
gegy1000 <gegy1000@gmail.com>
2015-03-27 07:52:40 +0200

    Mini Missiles

--- src/main/java/fiskfille/tf/common/network/MessageVurpSniperShoot.java
+++ src/main/java/fiskfille/tf/common/network/MessageVurpSniperShoot.java
@@ -81,8 +81,7 @@ public class MessageVurpSniperShoot implements IMessage
                                 TFNetworkManager.networkWrapper.sendToAllAround(new MessageVurpSniperShoot(player), new TargetPoint(player.dimension, player.posX, player.posY, player.posZ, 64));
                                 
                                 World world = player.worldObj;
-                                EntityMiniMissile entity = new EntityMiniMissile(world, player, 30, TFConfig.allowMissileExplosions);
-                                entity.setDamage(0.01D);
+                                EntityMiniMissile entity = new EntityMiniMissile(world, player, TFConfig.allowMissileExplosions);
                                 --entity.posY;
                                 world.spawnEntityInWorld(entity);
                                 
