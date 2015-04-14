400280df4f0a3f5429423c19d4ad46a5a581cc9b
gegy1000 <gegy1000@gmail.com>
2015-04-14 14:39:27 +0200

    Deerrp Fisk
    With my other attempts at fixing the bug.. .-.

--- src/main/java/fiskfille/tf/common/entity/EntityMissile.java
+++ src/main/java/fiskfille/tf/common/entity/EntityMissile.java
@@ -1,29 +1,34 @@
 package fiskfille.tf.common.entity;
 
+import io.netty.buffer.ByteBuf;
+import cpw.mods.fml.common.registry.IEntityAdditionalSpawnData;
+import net.minecraft.block.Block;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.monster.EntityBlaze;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.projectile.EntityThrowable;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.DamageSource;
 import net.minecraft.util.MovingObjectPosition;
 import net.minecraft.world.World;
 
-public class EntityMissile extends EntityThrowable
+public class EntityMissile extends EntityThrowable implements IEntityAdditionalSpawnData
 {
-	public boolean isInStealthMode;
-	public boolean allowExplosions;
-	
+    public boolean isInStealthMode;
+    public boolean allowExplosions;
+    
     public EntityMissile(World world)
     {
         super(world);
     }
-
-    public EntityMissile(World world, EntityLivingBase entity, boolean stealthMode, boolean explosions)
+    
+    public EntityMissile(World world, EntityLivingBase entity, boolean explosions, boolean stealthMode)
     {
         super(world, entity);
         isInStealthMode = stealthMode;
         allowExplosions = explosions;
     }
-
+    
     public EntityMissile(World world, double x, double y, double z)
     {
         super(world, x, y, z);
@@ -31,18 +36,18 @@ public class EntityMissile extends EntityThrowable
     
     public void onUpdate()
     {
-    	super.onUpdate();
-    	
-    	if (ticksExisted < 10 && !isInStealthMode)
-    	{
-    		posY -= 0.2F;
-    	}
-    	
-    	for (int i = 0; i < 20; ++i)
-    	{
-    		float spread = (rand.nextFloat() - 0.5F) / 4;
-    		worldObj.spawnParticle("smoke", posX + spread, posY + spread, posZ + spread, 0, 0, 0);
-    	}
+        super.onUpdate();
+        
+        if (ticksExisted < 10 && !isInStealthMode)
+        {
+            posY -= 0.2F;
+        }
+        
+        for (int i = 0; i < 20; ++i)
+        {
+            float spread = (rand.nextFloat() - 0.5F) / 4;
+            worldObj.spawnParticle("smoke", posX + spread, posY + spread, posZ + spread, 0, 0, 0);
+        }
     }
     
     protected float getGravityVelocity()
@@ -59,16 +64,52 @@ public class EntityMissile extends EntityThrowable
     {
         if (!worldObj.isRemote)
         {
-        	if (mop.typeOfHit == mop.typeOfHit.BLOCK)
-        	{
-        		worldObj.createExplosion(null, mop.blockX, mop.blockY, mop.blockZ, 4, allowExplosions);
-        	}
-        	else if (mop.typeOfHit == mop.typeOfHit.ENTITY)
-        	{
-        		worldObj.createExplosion(null, mop.entityHit.posX, mop.entityHit.posY, mop.entityHit.posZ, 4, allowExplosions);
-        	}
-        	
-            setDead();
+            if (mop.typeOfHit == mop.typeOfHit.BLOCK)
+            {
+                worldObj.createExplosion(null, mop.blockX, mop.blockY, mop.blockZ, 4, allowExplosions);
+            }
+            else if (mop.typeOfHit == mop.typeOfHit.ENTITY)
+            {
+                worldObj.createExplosion(null, mop.entityHit.posX, mop.entityHit.posY, mop.entityHit.posZ, 4, allowExplosions);
+            }
         }
+        
+        setDead();
+    }
+    
+    /**
+     * (abstract) Protected helper method to write subclass entity data to NBT.
+     */
+    public void writeEntityToNBT(NBTTagCompound nbt)
+    {
+        super.writeEntityToNBT(nbt);
+        
+        nbt.setBoolean("Explosions", allowExplosions);
+        nbt.setBoolean("StealthForce", isInStealthMode);
+    }
+    
+    /**
+     * (abstract) Protected helper method to read subclass entity data from NBT.
+     */
+    public void readEntityFromNBT(NBTTagCompound nbt)
+    {
+        super.readEntityFromNBT(nbt);
+        
+        allowExplosions = nbt.getBoolean("Explosions");
+        isInStealthMode = nbt.getBoolean("StealthForce");
+    }
+    
+    @Override
+    public void writeSpawnData(ByteBuf buf)
+    {
+        buf.writeBoolean(allowExplosions);
+        buf.writeBoolean(isInStealthMode);
+    }
+    
+    @Override
+    public void readSpawnData(ByteBuf buf)
+    {
+        allowExplosions = buf.readBoolean();
+        isInStealthMode = buf.readBoolean();
     }
 }
