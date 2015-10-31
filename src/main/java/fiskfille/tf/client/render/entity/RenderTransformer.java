7c8682a9026eb3ee8324611ae4c899879f9f0d71
gegy1000 <gegy1000@gmail.com>
2015-10-31 08:55:56 +0200

    testing ai on this

--- src/main/java/fiskfille/tf/client/render/entity/RenderTransformer.java
+++ src/main/java/fiskfille/tf/client/render/entity/RenderTransformer.java
@@ -2,12 +2,13 @@ package fiskfille.tf.client.render.entity;
 
 import fiskfille.tf.TransformersMod;
 import fiskfille.tf.client.model.transformer.ModelTransformer;
+import net.minecraft.client.renderer.entity.RenderLiving;
 import net.minecraft.client.renderer.entity.RendererLivingEntity;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.util.ResourceLocation;
 
-public class RenderTransformer extends RendererLivingEntity
+public class RenderTransformer extends RenderLiving
 {
     public RenderTransformer()
     {
@@ -18,11 +19,5 @@ public class RenderTransformer extends RendererLivingEntity
     protected ResourceLocation getEntityTexture(Entity entity)
     {
         return new ResourceLocation(TransformersMod.modid + ":textures/models/purge/purge.png");
-    }
-
-    @Override
-    public void doRender(EntityLivingBase entity, double x, double y, double z, float yaw, float partialTicks)
-    {
-        super.doRender(entity, x, y, z, yaw, partialTicks);
     }
 }
